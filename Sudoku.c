2a2b215bf12815eac2fd8ce8cf9fb1367a4583e7
avinash-vk <avinash2000vk@gmail.com>
2020-08-06 21:15:02 +0530

    added python+c

--- Sudoku.c
+++ Sudoku.c
@@ -1,21 +1,42 @@
 #include "sudoku_func.h"
-int main(){
-    int grid[9][9]={0};
-    generator_main(grid);
-    printf("Generated 2D Array Puzzle with 40 empty cells \n");
-    print(grid);
-    Sudoku* sudoku = initialize_board(); 
-    int d,i,j;
-    for(i=0;i<size;i++)
-    {
-        for(j=0;j<size;j++)
-        {
+
+void sudoku_to_array(Sudoku* sudoku, int grid[9][9]){
+    for(int i=0;i<9;i++){
+        for(int j=0;j<9;j++){
+            grid[i][j] = sudoku->vertices[i][j]->value;
+        }
+    }
+}
+
+void array_to_sudoku(Sudoku* sudoku, int grid[9][9]){
+    for(int i=0;i<9;i++){
+        for(int j=0;j<9;j++){
             sudoku->vertices[i][j]->value = grid[i][j];
         }
     }
+}
+
+void generator(int grid[9][9]){
+    generator_main(grid);
+    printf("Generated 2D Array Puzzle with 40 empty cells \n");
+    //print(grid);
+    //return grid;
+}
+
+void solver(int grid[9][9]){
+    Sudoku* sudoku = initialize_board(); 
+    int d,i,j;
+    array_to_sudoku(sudoku,grid);
     solve_Sudoku(sudoku);
     printf("Puzzle solved using graphs\n");
     display_board(sudoku);
+    sudoku_to_array(sudoku,grid);
+    //return grid;
+}
 
+int main(){ 
+    /*int grid[9][9]={0};
+    generator(grid);
+    print(grid);*/
     return 0;
 }
--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from starlette.middleware.cors import CORSMiddleware
+import c_utils
 
 app = FastAPI()
 
-@app.get("/")
+app.add_middleware(CORSMiddleware,allow_origins=['*'])
+app.mount("/",StaticFiles( directory = "frontend/public" ),name = "static")
+
+@app.get("/api/test")
 def home():
     return { "data" : "hello" }
+
+@app.get("/api/generate")
+def generate_sudoku():
+    grid = c_utils.generate_grid()
+    return { "data" : str(grid) }
+
+
